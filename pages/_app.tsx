1f7da6d37cd6bb4aaa47de71cf7d3c36c012c923
Lưu Công Minh (UPP DEL) <minhlc@upp-technology.com>
2023-05-25 14:20:10 +0700

    fix GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,25 +4,31 @@ import { ThemeProvider } from '@material-tailwind/react';
 import { AnimatePresence } from 'framer-motion';
 import { Router } from 'next/router';
 import { useEffect, useState } from 'react';
+import * as gtag from '../lib/gtag';
 
 export default function App({ Component, pageProps, router }: AppProps) {
   // const [loading, setLoading] = useState(false);
-  // useEffect(() => {
-  //   const start = () => {
-  //     setLoading(true);
-  //   };
-  //   const end = () => {
-  //     setLoading(false);
-  //   };
-  //   Router.events.on('routeChangeStart', start);
-  //   Router.events.on('routeChangeComplete', end);
-  //   Router.events.on('routeChangeError', end);
-  //   return () => {
-  //     Router.events.off('routeChangeStart', start);
-  //     Router.events.off('routeChangeComplete', end);
-  //     Router.events.off('routeChangeError', end);
-  //   };
-  // }, []);
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+
+    // const start = () => {
+    //   setLoading(true);
+    // };
+    // const end = () => {
+    //   setLoading(false);
+    // };
+    // Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    // Router.events.on('routeChangeError', end);
+    return () => {
+      // Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', handleRouteChange);
+      // Router.events.off('routeChangeError', end);
+    };
+  }, []);
 
   return (
     <ThemeProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,25 +10,22 @@ export default function Document() {
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
         />
         <Script
-          id='gtm-script'
           strategy='afterInteractive'
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id=G-G95D14B3PE'+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','G-G95D14B3PE');`,
-          }}
-        ></Script>
+          src='https://www.googletagmanager.com/gtag/js?id=G-G95D14B3PE'
+        />
+        <Script defer id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G95D14B3PE', {
+              page_path: window.location.pathname,
+            });
+              
+          `}
+        </Script>
       </Head>
       <body>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-G95D14B3PE"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        ></noscript>
-
         <Main />
         <NextScript />
       </body>

3956ec0e7d60ff8e87952edb6c9947116f91d8a4
Lưu Công Minh (UPP DEL) <minhlc@upp-technology.com>
2023-05-25 13:41:25 +0700

    add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -8,8 +9,26 @@ export default function Document() {
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
         />
+        <Script
+          id='gtm-script'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id=G-G95D14B3PE'+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','G-G95D14B3PE');`,
+          }}
+        ></Script>
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-G95D14B3PE"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
+
         <Main />
         <NextScript />
       </body>
